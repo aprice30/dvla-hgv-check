319254db3e4701feacd1962cb1ffb0951d4ebc80
Adam Price <aprice30@gmail.com>
2024-07-20 14:23:08 +0100

    Moved code inside Motion processor to a new thread

--- motion_detector/motionprocessor.py
+++ motion_detector/motionprocessor.py
@@ -2,6 +2,7 @@ import logging
 import ffmpegcv # type: ignore
 import threading
 from collections import deque
+from queue import Queue
 from datetime import datetime
 
 logger = logging.getLogger(__name__)
@@ -12,20 +13,34 @@ class MotionProcessorInterface:
 
 class MotionProcessor(MotionProcessorInterface):
     def __init__(self, outputPath):
-        self.outputPath = outputPath
+        self.outputPath: str = outputPath
+        self.queue = Queue()
 
-        self.buffer = deque()
+        # Start a thread to process the buffer in the background
+        t = threading.Thread(target=self.processQueue, args=(), daemon=True)
+        t.start()
     
-    def process(self, timestamp: datetime, frameCount: int, buffer: deque):
+    def process(self, timestamp: datetime, frameCount: int, frames: deque):
         # Create the filename
         formatted_time = timestamp.strftime("%Y-%m-%d_%H-%M-%S")
         fileName = "{}/{}_{}.mp4".format(self.outputPath, formatted_time, frameCount)
-        logger.info("Saving motion to %s", fileName)
+        
+        payload = MotionPayload(fileName, frames.copy())
+        self.queue.put(payload)
 
-        try:
+    def processQueue(self):
+        payload: MotionPayload
+        for payload in iter(self.queue.get, None): # Replace `None` as you need.
+            logger.info("Saving motion to %s", payload.fileName)
 
-            with ffmpegcv.VideoWriter(fileName, None, 10) as out:
-                for frame in buffer:
-                 out.write(frame)
-        except:
-            logger.exception("Failed to save clip")
+            try:
+                with ffmpegcv.VideoWriter(payload.fileName, None, 10) as out:
+                    for frame in payload.frames:
+                        out.write(frame)
+            except:
+                logger.exception("Failed to save clip")
+
+class MotionPayload:
+    def __init__(self, fileName: str, frames: deque):
+        self.fileName: str = fileName
+        self.frames: deque = frames
